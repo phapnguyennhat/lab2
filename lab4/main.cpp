2d57694aeb49e0a9b1d3e2671d2910df9bcb388e
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2023-11-23 23:25:08 +0700

    lab5

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,118 +4,20 @@
 
 using namespace std;
 
-template <class K, class D, int M> // K: key, D: data, M: degree of BTree
-class BTree {
-    /// Convention: Left sub-tree < Root's key <= Right sub-tree
-
+class PrinterQueue
+{
+    // your attributes
 public:
-    class Entry;
+    // your methods
 
-    class Node;
-
-private:
-    Node *root;
-
-public:
-    BTree() : root(0) {};
-
-    ~BTree() {}
-    ///////////////////////////////////////////////////
-    ///             CLASS `Entry`                   ///
-    ///////////////////////////////////////////////////
-public:
-    class Entry {
-    private:
-        K key;
-        D data;
-        Node *rightPtr;
-
-        friend class BTree<K, D, M>;
-
-    public:
-        Entry(K key = K{}, D value = D{}) : key(key), data(value), rightPtr(0) {}
-        ~Entry() {}
-        string toString() {
-            stringstream ss;
-            ss << "<"
-                   << this->key << ","
-                   << this->data
-                   << ">";
-            return ss.str();
-        }
-    };
-
-    ///////////////////////////////////////////////////
-    ///             CLASS `Node`                    ///
-    ///////////////////////////////////////////////////
-public:
-    class Node {
-    private:
-        Node *firstPtr;
-        int numEntries;
-        Entry entries[M - 1];
-
-        friend class BTree<K, D, M>;
-
-    public:
-        Node() : firstPtr(0), numEntries(0) {};
-
-        ~Node() { }
-        string toString() {
-            stringstream ss;
-            // Fill your own code here
-            return ss.str();
-        }
-        bool isFull() {
-            return (numEntries >= M - 1);
-        }
-    };
-
-    /////////////////////////////////////////////////////////////
-    ///         CLASS `BTree`: method implementation          ///
-    /////////////////////////////////////////////////////////////
-public:
-    /// toStringPreOrder()
-    string toStringPreOrder();
-    /// BEGIN STUDENT CODE
-/// BEGIN STUDENT CODE
-/// =====================================================
-
-/// Proposal methods from slides
-/// Feel free to use them,
-///     or delete them and implement your own helping methods
-    void insertEntry(Node *pNode, int entryIdx, const Entry &newEntry)
+    void addNewRequest(int priority, string fileName)
     {
-      
+        // your code here
     }
 
-    void splitNode(Node *pNode, int entryIdx, Entry &upEntry)
+    void print()
     {
-        
+        // your code here
+        // After some logic code, you have to print fileName with endline 
     }
-
-    int searchNode(Node *nodePtr, K key)
-    {
-      for(int i=0;i<nodePtr->numEntries;i++){
-        if(key==nodePtr->entries[i].key) return i;
-      }
-      return -1;
-    }
-
-    bool insertNode(Node *&root, const K &key, const D &data, Entry &upEntry)
-    {
-    }
-
-    void insert(Node *&root, const K &key, const D &data)
-    {
-    }
-
-    /// TODO: Implement following method
-    void insert(const K &key, const D &data)
-    {
-        
-    }
-
-/// END STUDENT CODE
-    /// END STUDENT CODE
 };
